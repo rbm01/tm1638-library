e443860cf2c8b9e929cf2adebb08226ca150a84b
Robert Mills <rmills@gigabyte.homenet1.au>
2018-10-21 18:16:15 +1100

    Add #includes to cover PROGMEM macro and associated functions.

--- TM16XX.h
+++ TM16XX.h
@@ -25,6 +25,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	#include "WProgram.h"
 #endif
 
+#ifdef ARDUINO
+	#include <avr/pgmspace.h>
+#else
+	#define PROGMEM
+	#define pgm_read_byte(a) (*(a))
+#endif /* ARDUINO */
+
 #include "TM16XXFonts.h"
 
 class TM16XX
